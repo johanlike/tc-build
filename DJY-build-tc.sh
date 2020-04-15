90e05f57aac5e8ce8f33ea74d169517e73b691be
Johanlike <17888022+johanlike@users.noreply.github.com>
2020-04-15 19:45:01 -0400

    Fully optimized script settings And specify the installation path
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- DJY-build-tc.sh
+++ DJY-build-tc.sh
@@ -8,15 +8,18 @@ function msg() {
     echo -e "\e[1;32m$@\e[0m"
 }
 
+#Install Clang Path
+install=~/Desktop/Clang
+
 #First run scripts need install dependencies
 msg "Do you need to install related dependencies? [Y/n]"
 read -r -p : input
 
 case $input in
 	    [yY][eE][sS]|[yY])
-			msg "You select Yes now install dependencies"
-			apt update
-			apt install bc \
+			msg "You select Yes now $install dependencies"
+			  apt update
+			  apt install bc \
               bison \
               ca-certificates \
               ccache \
@@ -65,58 +68,34 @@ msg "Building LLVM..."
 	--incremental \
 	--build-stage1-only \
 	--install-stage1-only \
-	--install-folder "installTmp" \
+	--install-folder "$install" \
 
 # Build binutils
 msg "Building binutils..."
-#if [ $(which clang) ] && [ $(which clang++) ]; then
-#	export CC=$(which ccache)" clang"
-#	export CXX=$(which ccache)" clang++"
-#	[ $(which llvm-strip) ] && stripBin=llvm-strip-9
-#else
-#	export CC=$(which ccache)" gcc"
-#	export CXX=$(which ccache)" g++"
-#	[ $(which strip) ] && stripBin=strip
-#fi
 ./build-binutils.py \
 	--targets arm aarch64 x86_64 \
-	--install-folder "installTmp"
+	--install-folder "$install"
 
 # Remove unused products
 msg "Removing unused products..."
-rm -f installTmp/lib/*.a installTmp/lib/*.la installTmp/lib/clang/*/lib/linux/*.a*
+rm -f $install/lib/*.a $install/lib/*.la $install/lib/clang/*/lib/linux/*.a* 
+rm -rf $install/include
 
-msg "Setting library load paths for portability and"
+
+# Strip remaining products
 msg "Stripping remaining products..."
-IFS=$'\n'
-for f in $(find installTmp -type f -exec file {} \;); do
-	if [ -n "$(echo $f | grep 'ELF .* interpreter')" ]; then
-		i=$(echo $f | awk '{print $1}'); i=${i: : -1}
-		# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
-		if [ -d $(dirname $i)/../lib/ldscripts ]; then
-			patchelf --set-rpath '$ORIGIN/../../lib:$ORIGIN/../lib' "$i"
-		else
-			if [ "$(patchelf --print-rpath $i)" != "\$ORIGIN/../../lib:\$ORIGIN/../lib" ]; then
-				patchelf --set-rpath '$ORIGIN/../lib' "$i"
-			fi
-		fi
-		# Strip remaining products
-		stripBin=llvm-strip-9
-		if [ -n "$(echo $f | grep 'not stripped')" ]; then
-			${stripBin} --strip-unneeded "$i"
-		fi
-	elif [ -n "$(echo $f | grep 'ELF .* relocatable')" ]; then
-		if [ -n "$(echo $f | grep 'not stripped')" ]; then
-			i=$(echo $f | awk '{print $1}');
-			${stripBin} --strip-unneeded "${i: : -1}"
-		fi
-	else
-		if [ -n "$(echo $f | grep 'not stripped')" ]; then
-			i=$(echo $f | awk '{print $1}');
-			${stripBin} --strip-all "${i: : -1}"
-		fi
-	fi
+for f in $(find $install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
+	strip ${f: : -1}
 done
 
-rm -rf ./install
-mv installTmp/ ~/Desktop/Clang
+# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
+msg "Setting library load paths for portability..."
+for bin in $(find $install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
+	# Remove last character from file output (':')
+	bin="${bin: : -1}"
+
+	echo "$bin"
+	patchelf --set-rpath '$ORIGIN/../lib' "$bin"
+done
+
+
